472f336173594b3edd21067fce9ea864352fae03
mschodin <michael-schodin@uiowa.edu>
2020-11-04 12:28:09 -0600

    Generated rspec, moved spec tests to spec folder

---
+++ spec/controllers/room_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe RoomController do
+  describe 'creating a room' do
+    it 'should create a new room when new room controller path is called' do
+      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
+      expect(Room).to receive(:create!).with(unique_id)
+      get :new_room_path
+    end
+  end
+end
