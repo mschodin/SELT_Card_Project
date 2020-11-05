8c2bb45153a4bddba89af7366fb4a12cff563578
mschodin <michael-schodin@uiowa.edu>
2020-11-05 12:36:28 -0600

    Added rspec tests for creating a room

--- spec/controllers/room_controller_spec.rb
+++ spec/controllers/room_controller_spec.rb
@@ -8,5 +8,16 @@ describe RoomController do
       expect(Room).to receive(:create!).with([id: unique_id.to_i])
       get :new
     end
+    it 'should redirect to the newly created room when a new room is requested' do
+      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
+      expect(room_path(unique_id)).to eq('/room/' + unique_id)
+      get :new
+    end
+    it 'should create a new room with the correct id number created from current time' do
+      get :new
+      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
+      created_room = Room.find(unique_id.to_i)
+      expect(created_room.id).to eq(unique_id.to_i)
+    end
   end
 end
