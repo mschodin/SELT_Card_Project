e7b720d51c784c3c949fb73b0fe03cff3789880d
awashington <antonio-washington@uiowa.edu>
2020-11-05 22:24:15 -0600

    db generation and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_173839) do
+ActiveRecord::Schema.define(version: 2020_11_06_011503) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "deck_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deck_id"], name: "index_cards_on_deck_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_decks_on_room_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
   end
