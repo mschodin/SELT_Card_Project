e80e4f951d5aa019fe9ea65a718bf3a1ab92ec40
senortonito <senortonito@gmail.com>
2020-11-05 22:10:13 -0600

    add sub routes for deck

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  # get 'Deck/new'
+  # get 'Deck/draw'
+  # get 'Deck/show'
+  # get 'Deck/create'
+  # get 'Deck/delete'
+  # get 'Deck/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :room
+  resources :room do
+    resources :deck
+  end
   root :to => redirect('/room')
 end
