1aefcb58763faec534f21b99dde0a76f37def51f
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-11-09 09:33:52 -0600

    Adding deck#draw route for drawing a card

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # get 'Deck/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :room do
-    resources :deck
+    resources :deck do
+      get 'draw', to:'deck#draw'
+    end
   end
   root :to => redirect('/room')
 end
