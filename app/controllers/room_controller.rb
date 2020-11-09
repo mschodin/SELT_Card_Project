363b63b15292b59dd7826387e9985808ecef24d1
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-11-09 09:36:51 -0600

    Added room_id to session, added logic to build a deck of cards to be referenced in the room#show view

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -8,12 +8,26 @@ class RoomController < ApplicationController
 
   def new
     unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
-    Room.create!([:id => unique_id.to_i])
+    session[:room_id] = unique_id
+    @room = Room.create!([:id => unique_id.to_i])
     redirect_to room_path(unique_id.to_i)
   end
 
   def show
+    @room = get_room()
+    if @room.nil?
 
+    else
+      items = @room.cards.where(params[:id])
+      @room_items = {}
+      items.each do |card|
+        @room_items[card['deck_id']].nil? ? @room_items[card['deck_id']] = [{:suit => card['suit'],:rank=> card['rank']}] : @room_items[card['deck_id']] << {:suit => card['suit'],:rank => card['rank']}
+      end
+    end
+  end
+
+  def get_room
+    @room = Room.find(session[:room_id])
   end
 
 end
