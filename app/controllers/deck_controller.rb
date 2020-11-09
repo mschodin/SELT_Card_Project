5541f030c585e2a1a72949d627da21d1ca1a0b5b
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-11-09 09:37:44 -0600

    Added logic to shuffle deck before writing to database, added logic for drawing 1 card

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -3,6 +3,15 @@
 
 class DeckController < ApplicationController
   def draw
+    @room = Room.find(session[:room_id])
+    items = @room.cards.where(session[:room_id])
+    @room_items = {}
+    items.each do |card|
+      @room_items[card['deck_id']].nil? ? @room_items[card['deck_id']] = [{:suit => card['suit'],:rank=> card['rank']}] : @room_items[card['deck_id']] << {:suit => card['suit'],:rank => card['rank']}
+    end
+    @draw_card = @room_items[params['deck_id'].to_i].pop
+    deck = Deck.find(params['deck_id'])
+    deck.cards.find_by(suit: @draw_card[:suit], rank: @draw_card[:rank]).update(deck_id: nil)
   end
 
   def show
@@ -12,6 +21,7 @@ class DeckController < ApplicationController
     @room = Room.find(params[:room_id])
     deck_db = @room.decks.create({:room_id => params[:room_id]}) #create instead of build because only attribute is room_id
     @deck = Deck.create_deck
+    @deck.shuffle!
     @deck.each do |card|
       deck_db.cards.create(card)
     end
