77b2af1872b0230acf735313641cc6885c15a96d
mschodin <michael-schodin@uiowa.edu>
2020-11-09 21:24:29 -0600

    really not sure what I'm doing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # get 'Deck/new'
-  # get 'Deck/draw'
-  # get 'Deck/show'
-  # get 'Deck/create'
-  # get 'Deck/delete'
-  # get 'Deck/index'
+  # get 'deck/new'
+  # get 'deck/draw'
+  # get 'deck/show'
+  # get 'deck/create'
+  # get 'deck/delete'
+  # get 'deck/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :room do
     resources :deck do
