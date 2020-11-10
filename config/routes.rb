4077ff233b5a93fb7569f2ab99f9f37843196d0a
mschodin <michael-schodin@uiowa.edu>
2020-11-09 21:30:00 -0600

    Changing naming convention fixed heroku deployment, reverted minor testing changes before merging

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # get 'deck/new'
-  # get 'deck/draw'
-  # get 'deck/show'
-  # get 'deck/create'
-  # get 'deck/delete'
-  # get 'deck/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :room do
     resources :deck do
