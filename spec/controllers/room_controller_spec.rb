598a323d23ee4469815e7e291a0f298c3e4c9bce
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-11-10 18:31:56 -0600

    Fixes for RoomController tests

--- spec/controllers/room_controller_spec.rb
+++ spec/controllers/room_controller_spec.rb
@@ -4,20 +4,22 @@ require 'spec_helper'
 describe RoomController do
   describe 'creating a room' do
     it 'should create a new room when new room controller path is called' do
-      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
-      expect(Room).to receive(:create!).with([id: unique_id.to_i])
+      unique_id = 1
+      expect(Room).to receive(:create!).with(no_args)
+      room = assigns(:room)
+      allow(room).to receive(:id).and_return(unique_id)
       get :new
     end
     it 'should redirect to the newly created room when a new room is requested' do
-      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
+      unique_id = '1'
       expect(room_path(unique_id)).to eq('/room/' + unique_id)
       get :new
     end
     it 'should create a new room with the correct id number created from current time' do
       get :new
-      unique_id = DateTime.now.strftime "%y%m%d%H%M%S"
-      created_room = Room.find(unique_id.to_i)
-      expect(created_room.id).to eq(unique_id.to_i)
+      unique_id = 1
+      created_room = Room.find(unique_id)
+      expect(created_room.id).to eq(unique_id)
     end
   end
   describe 'showing a room and its contents' do
