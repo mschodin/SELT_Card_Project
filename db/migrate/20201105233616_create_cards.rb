1b268ea773281f3c663449bf74363a14034a8f74
senortonito <senortonito@gmail.com>
2020-11-11 17:06:19 -0600

    migrate changes to match game hand application record

--- db/migrate/20201105233616_create_cards.rb
+++ db/migrate/20201105233616_create_cards.rb
@@ -2,6 +2,7 @@ class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
       t.references :deck
+      t.references :gamehand
       t.string :rank
       t.string :suit
       t.timestamps
