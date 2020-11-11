f10b5776ee5ee7f61ff8d7245ebed4f12e0677ff
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-11-10 22:47:14 -0600

    Added cucumber scenario tests for drawing a card and for when the draw button should be hidden

--- features/step_definitions/deck_steps.rb
+++ features/step_definitions/deck_steps.rb
@@ -16,3 +16,28 @@ Then(/^I should see that a new deck is in the room$/) do
   expect(cards).to include match "Hearts"
   expect(cards).to include match "Club"
 end
+
+
+Given(/^I am on the game room home page AND a deck has been created$/) do
+  visit room_index_path
+  click_button('Create a game')
+  click_button('New Deck')
+end
+
+When(/^I click Draw (\d+) Card$/) do |arg|
+  click_button('Draw 1 Card')
+end
+
+Then(/^I should see the card that was drawn$/) do
+  cards =  page.all('td').map { |td| td.text }
+  expect(cards).to include match "Suit"
+  expect(cards).to include match "Rank"
+  suits = %w[Diamonds Spades Clubs Hearts]
+  ranks = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace]
+  expect(suits).to include cards[-1]
+  expect(ranks).to include cards[2]
+end
+
+Then(/^I should not see the Draw (\d+) Card button$/) do |arg|
+  expect(page).not_to have_button('Draw 1 Card')
+end
