986d352e374b42d4e82e6051c70d3edf612e06e2
senortonito <antonio-washington@uiowa.edu>
2020-11-11 16:56:54 -0600

    add player function added to player model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,12 @@
 class Room < ActiveRecord::Base
   has_many :players
   has_many :decks
+  has_many :game_hands, through: :players
   has_many :cards, through: :decks
+
+  def add_player name
+    player = self.players.create(:name => name)
+    player.hand.create
+    player
+  end
 end
