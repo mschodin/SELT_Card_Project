a00b51a19b033f1a01e63bb354897c96b6163c97
mschodin <michael-schodin@uiowa.edu>
2020-11-13 12:00:28 -0600

    Created cucumber tests for joining a room

--- features/step_definitions/room_steps.rb
+++ features/step_definitions/room_steps.rb
@@ -2,6 +2,11 @@ Given /^I am on the game room home page$/ do
   visit room_index_path
 end
 
+Given /^A room has been created$/ do
+  visit room_index_path
+  click_button('Create a game')
+end
+
 When /^I click create a new room$/ do
   click_button('Create a game')
 end
@@ -10,4 +15,34 @@ Then /^I should be placed in a new game room$/ do
   url = URI.parse(current_url)
   unique_id = '1'
   expect(url.path).to eq('/room/' + unique_id)
+end
+
+When /^I click join a room with name "(.*?)" and room id "(.*?)"$/ do |name, id|
+  page.fill_in "name_box", :with => name
+  page.fill_in 'room_id_box', :with => id
+  click_button('Join Game')
+end
+
+Then /^I should be placed in the game room with id "(.*?)"$/ do |id|
+  url = URI.parse(current_url)
+  unique_id = '1'
+  expect(url.path).to eq('/room/' + unique_id)
+end
+
+Then /^The user is notified their name is invalid$/ do
+  url = URI.parse(current_url)
+  expect(url.path).to eq('/room')
+  expect(flash[:notice]).to match(/Name is invalid, please try again/)
+end
+
+Then /^The user is notified the room id is invalid$/ do
+  url = URI.parse(current_url)
+  expect(url.path).to eq('/room')
+  expect(flash[:notice]).to match(/Room id invalid, please try again/)
+end
+
+Then /^The user is notified the room does not exist$/ do
+  url = URI.parse(current_url)
+  expect(url.path).to eq('/room')
+  expect(flash[:notice]).to match(/Room does not exist, please try again/)
 end
