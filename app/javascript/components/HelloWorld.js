b2a8520a64ea354d19cf9b5a2f03079a2d483151
awashington <antonio-washington@uiowa.edu>
2020-11-17 19:07:30 -0600

    most basic example to integrate a basic text component

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,14 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {MDCTopAppBar} from '@material/top-app-bar'
 
 class HelloWorld extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <MDCTopAppBar>
-
-        </MDCTopAppBar>
         Greeting: {this.props.greeting}
       </React.Fragment>
     );
