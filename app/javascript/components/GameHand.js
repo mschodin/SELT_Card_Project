691f115d0febcf8003a72804b888f8023f600f46
mschodin <michael-schodin@uiowa.edu>
2020-12-01 13:54:17 -0600

    Shopify react draggable is working well, I'm going to try react-dragable now

--- app/javascript/components/GameHand.js
+++ app/javascript/components/GameHand.js
@@ -1,25 +1,82 @@
 import React from "react"
 import PropTypes from "prop-types";
 import { Card, CardStyles } from 'react-casino';
-import Container from '@material-ui/core/Container';
+import {
+    DraggableItem,
+    DraggableContainer,
+    DroppableZone,
+} from '@wuweiweiwu/react-shopify-draggable';
 import '../../assets/stylesheets/application.css';
+import {blue} from "@material-ui/core/colors";
 
 class GameHand extends React.Component {
+
+
+
     render () {
         console.log(this.props.cards)
         console.log(this.props.piles)
+
         return (
             <React.Fragment>
-                {/*{this.props.cards[0][0]}*/}
-                {/*<Card suit="S" face="A" />*/}
-                <div id='player1Hand'>
-                    <div id='card1' className='pile'>
-                        <Card suit="D" face="9" style={{width: "75px", height: "108"}}/>
-                    </div>
-                    <div id='card2' className='pile'>
-                        <Card suit="S" face="A" style={{width: "75px", height: "108"}}/>
+                {/*<div id='player1Hand'>*/}
+                {/*    <div id='card1' className='pile'>*/}
+                {/*        <Card suit="D" face="9" style={{width: "75px", height: "108"}}/>*/}
+                {/*    </div>*/}
+                {/*    <div id='card2' className='pile'>*/}
+                {/*        <Card suit="S" face="A" style={{width: "75px", height: "108"}}/>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
+
+
+
+                <div className="App">
+                    <div className="App-body">
+
+                        <DraggableContainer
+                            as="div"
+                            type="swappable"
+                        >
+                            <DroppableZone
+                                as="div"
+                                type="draggable"
+                                className="pile">
+                                <DraggableItem
+                                    as="div"
+                                    style={{backgroundColor: blue}}
+                                >
+                                    <Card suit="S" face="9" style={{width: "75px", height: "108"}}/>
+                                </DraggableItem>
+                            </DroppableZone>
+                            <DroppableZone
+                                as="div"
+                                type="draggable"
+                                className="pile">
+                                <DraggableItem
+                                    as="div"
+                                    style={{backgroundColor: blue}}
+                                >
+                                    <Card suit="D" face="9" style={{width: "75px", height: "108"}}/>
+                                </DraggableItem>
+                            </DroppableZone>
+
+                            <DroppableZone
+                                as="div"
+                                type="droppable"
+                                className="pile">
+
+                            </DroppableZone>
+
+                        </DraggableContainer>
+
+
+
                     </div>
                 </div>
+
+
+
             </React.Fragment>
         );
     }
