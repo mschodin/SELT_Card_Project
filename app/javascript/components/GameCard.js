beb0f500d9d71e56692239db8d8ec736a604589f
mschodin <michael-schodin@uiowa.edu>
2020-12-01 15:42:40 -0600

    Created react GameCard

--- app/javascript/components/GameCard.js
+++ app/javascript/components/GameCard.js
@@ -1,14 +1,25 @@
 import React from "react"
+import { Draggable } from 'react-beautiful-dnd'
+import PropTypes from "prop-types"
+import { Card } from 'react-casino'
 
 class GameCard extends React.Component {
-
     render () {
         return (
-            <div>
-
-            </div>
+            <Draggable draggableId={this.props.cardId} index={this.props.index}>
+                {(provided, snapshot) => (
+                    <div ref={provided.innerRef}>
+                        <Card face={this.props.face} suit={this.props.suit} />
+                    </div>
+                )}
+            </Draggable>
         );
     }
 }
+
+GameCard.propTypes = {
+    cardId: PropTypes.string,
+    index: PropTypes.string
+};
 
 export default GameCard
