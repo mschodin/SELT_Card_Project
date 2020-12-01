1d9e1a8077c3c0e1c29b72c4ac7e0c2d50dabf3a
mschodin <michael-schodin@uiowa.edu>
2020-12-01 15:48:15 -0600

    Created hand and pile react components

--- app/javascript/components/GameHand.js
+++ app/javascript/components/GameHand.js
@@ -1,14 +1,25 @@
 import React from "react"
+import {Droppable} from "react-beautiful-dnd";
+import PropTypes from "prop-types";
+import GameCard from "./GameCard";
 
 class GameHand extends React.Component {
 
     render () {
         return (
-            <div>
-
-            </div>
+            <Droppable droppableId={this.props.handId} direction={"horizontal"}>
+                {(provided, snapshot) => (
+                    <div ref={provided.innerRef}>
+                        {/*  TODO: Create GameCard objects for each card in this hand  */}
+                    </div>
+                )}
+            </Droppable>
         );
     }
 }
+
+GameHand.propTypes = {
+    handId: PropTypes.string
+};
 
 export default GameHand
--- app/javascript/components/GamePile.js
+++ app/javascript/components/GamePile.js
@@ -1,14 +1,24 @@
 import React from "react"
+import { Droppable } from 'react-beautiful-dnd'
+import PropTypes from "prop-types";
 
 class GamePile extends React.Component {
 
     render () {
         return (
-            <div>
-
-            </div>
+            <Droppable droppableId={this.props.pileId} isCombineEnable={true}>
+                {(provided, snapshot) => (
+                    <div ref={provided.innerRef}>
+                    {/*  TODO: Create GameCard objects for each card in this pile  */}
+                    </div>
+                )}
+            </Droppable>
         );
     }
 }
+
+GamePile.propTypes = {
+    pileId: PropTypes.string
+};
 
 export default GamePile
