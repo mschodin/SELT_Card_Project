0a52b5cfaf81fb3e706af736e64e5353fdd0c22c
awashington <antonio-washington@uiowa.edu>
2020-12-03 15:27:46 -0600

    Render game table with piles and decks

--- app/javascript/components/GameTable.js
+++ app/javascript/components/GameTable.js
@@ -1,12 +1,25 @@
 import React from "react"
+import Box from "@material-ui/core/Box";
+import GameHand from "./GameHand";
+import Typography from "@material-ui/core/Typography";
+import GameCard from "./GameCard";
+import GamePile from "./GamePile";
 
 class GameTable extends React.Component {
 
     render () {
+        // console.log(this.props.piles)
         return (
-            <div>
-
-            </div>
+            <Box
+                pb={10}
+                className={"centered"}
+            >
+                <Box className={"tableStyle"} bgcolor={"primary.main"} boxShadow={5}>
+                    {this.props.piles.map((pile, idx) =>(
+                        <GamePile hidden={true} pileId={"pile" + pile[0]} key={"pile" + pile[0]} pileCards={pile[1]}/>
+                    ))}
+                </Box>
+            </Box>
         );
     }
 }
