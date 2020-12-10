097cf98683d46ee8c81677de7b17ff607dd29021
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-10 12:13:46 -0600

    Created testing file to test home appbar component

---
+++ spec/javascripts/homeappbar.spec.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import Home from "../../app/javascript/components/Home";
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+
+test("Check that the headers are shown", ()=>{
+
+
+
+});
