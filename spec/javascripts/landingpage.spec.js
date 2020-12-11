8f731dc33e10b5ee9732e83bdcd104603fd8f7ee
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-11 13:14:52 -0600

    WIP: First attempts at event handler testing

---
+++ spec/javascripts/landingpage.spec.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import HomeAppBar from "../../app/javascript/components/HomeAppBar";
+import { shallow, mount } from 'enzyme';
+import Home from "../../app/javascript/components/Home";
+import {TextField} from "@material-ui/core";
+
+test("Check that onChange updates text field", ()=>{
+    // const test_flash_message = null;
+    // const {room_join_url, room_index_url} = "";
+    // render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
+    //
+    // const onChangeMock = jest.fn();
+    // const event = {
+    //     target: { value: 'test' }
+    // }
+    // const component = mount<TextField>(<TextField onChange={onChangeMock} />);
+    // const instance = component.instance();
+    // instance.onChange(event);
+    // expect(onChangeMock).toBeCalledWith('test');
+});
+
+test('calls onSubmit prop function when form is submitted', () => {
+    // const onSubmitFn = jest.fn();
+    // const wrapper = mount(<form onSubmit={onSubmitFn}/>);
+    // const form = wrapper.find('form');
+    // form.simulate('submit');
+    // expect(onSubmitFn).toHaveBeenCalledTimes(1);
+});
