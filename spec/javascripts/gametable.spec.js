4888ab805629594ae835fa76cc94f1d8bb1d63df
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-11 13:15:17 -0600

    WIP: attempting to fix old broken test for gametable, wants to render but cant

--- spec/javascripts/gametable.spec.js
+++ spec/javascripts/gametable.spec.js
@@ -5,7 +5,8 @@ import GameRoom from "../../app/javascript/components/GameRoom";
 test('Check that GameTable exists', () => {
     const test_piles =  [[0,[["A", "S", 0],["A", "C", 1],["A", "H", 2]]],[ 1, [["A", "S", 0],["A", "C", 1],["A", "H", 2]]]]
     const test_hand =  [["A", "S", 0],["A", "C", 1],["A", "H", 2]]
-    const { getAllByRole, getByText, getByLabelText, getByTestId} = render(<GameRoom handId={0} playerHand={test_hand} piles={test_piles} />);
+    let test_players = [{"test":0}]
+    const { getAllByRole, getByText, getByLabelText, getByTestId} = render(<GameRoom handId={0} playerHand={test_hand} piles={test_piles} players={test_players}/>);
     expect(getAllByRole('table').length).toBe(1)
     expect(getAllByRole('pile').length).toBe(test_piles.length)
 });
