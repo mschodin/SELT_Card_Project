db083da0cc4e97506e299dc3445c369267364499
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-11 13:13:19 -0600

    Added enzyme to try event handler testing

--- spec/javascripts/homeappbar.spec.js
+++ spec/javascripts/homeappbar.spec.js
@@ -1,10 +1,9 @@
 import * as React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import Home from "../../app/javascript/components/Home";
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import HomeAppBar from "../../app/javascript/components/HomeAppBar";
 
-test("Check that the headers are shown", ()=>{
-
-
-
+test("Check that the homepage headers are shown", ()=>{
+    render(<HomeAppBar/>);
+    expect(screen.getByText('Online Cards')).toBeInTheDocument();
 });
