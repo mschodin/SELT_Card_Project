0e4c99103f90f92a9198624fd3c5d4125f7dc29f
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-11 13:13:49 -0600

    Changed test structures to fit the describe, it should structure of BDD

--- spec/javascripts/flashmessage.spec.js
+++ spec/javascripts/flashmessage.spec.js
@@ -3,19 +3,22 @@ import '@testing-library/jest-dom/extend-expect'
 import Home from "../../app/javascript/components/Home";
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 
-test('Check that FlashMessage Displays', () => {
-    const test_flash_message = "This is a test message";
-    const {room_join_url, room_index_url} = "";
-    const{ getByText } = render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
+describe(`showing user alert messages`, ()=> {
 
-    expect(screen.getByRole('alert')).toBeInTheDocument();
-    expect(screen.getByRole('alert')).toHaveTextContent(test_flash_message);
-});
+    it('Should display a FlashMessage', () => {
+        const test_flash_message = "This is a test message";
+        const {room_join_url, room_index_url} = "";
+        const {getByText} = render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
 
-test('Check that FlashMessage does not display', () => {
-    const test_flash_message = null;
-    const {room_join_url, room_index_url} = "";
-    const{ getByText } = render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
+        expect(screen.getByRole('alert')).toBeInTheDocument();
+        expect(screen.getByRole('alert')).toHaveTextContent(test_flash_message);
+    });
 
-    expect(screen.queryByRole('alert')).toBeNull()
+    it('Should not display a FlashMessage', () => {
+        const test_flash_message = null;
+        const {room_join_url, room_index_url} = "";
+        const {getByText} = render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
+
+        expect(screen.queryByRole('alert')).toBeNull()
+    });
 });
