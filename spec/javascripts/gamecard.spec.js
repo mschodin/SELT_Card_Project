587f0c681ed71800197bfe7ebfa98441d948df46
mschodin <michael-schodin@uiowa.edu>
2020-12-12 14:14:27 -0600

    Fixed card tests, still writing pile tests

--- spec/javascripts/gamecard.spec.js
+++ spec/javascripts/gamecard.spec.js
@@ -3,12 +3,19 @@ import GameCard from "../../app/javascript/components/GameCard";
 import {DragDropContext, Droppable} from "react-beautiful-dnd";
 import {cleanup, render} from "@testing-library/react";
 
+afterEach(cleanup);
+
 describe('Check the card is rendered correctly', () => {
-    it('renders the card as an image', () => {
+    it('renders the GameCard object', () => {
         const {container, getByAltText} = render(
             <DragDropContext onDragEnd={() => {}}>
-                <Droppable>
-                    <GameCard face={'2'} suit={'C'} cardId={'card1'} index={1} key={'card1'}/>
+                <Droppable droppableId={"1"}>
+                    {(provided, snapshot) => (
+                        <div ref={provided.innerRef}>
+                            <GameCard face={'2'} suit={'C'} cardId={'card1'} index={1} key={'card1'}/>
+                            {provided.placeholder}
+                        </div>
+                    )}
                 </Droppable>
             </DragDropContext>
         )
@@ -18,8 +25,13 @@ describe('Check the card is rendered correctly', () => {
     it('renders the card as an image', () => {
         const {container, getByAltText} = render(
             <DragDropContext onDragEnd={() => {}}>
-                <Droppable>
-                    <GameCard face={'A'} suit={'S'} cardId={'card1'} index={1} key={'card1'}/>
+                <Droppable droppableId={"1"}>
+                    {(provided, snapshot) => (
+                        <div ref={provided.innerRef}>
+                            <GameCard face={'A'} suit={'S'} cardId={'card1'} index={1} key={'card1'}/>
+                            {provided.placeholder}
+                        </div>
+                    )}
                 </Droppable>
             </DragDropContext>
         )
--- spec/javascripts/gamepile.spec.js
+++ spec/javascripts/gamepile.spec.js
@@ -1,7 +1,23 @@
 import * as React from "react";
-import {render} from "@testing-library/react";
 import GamePile from "../../app/javascript/components/GamePile";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import {cleanup, render} from "@testing-library/react";
 
-test('Check that a GamePile exists', () => {
-
-});
+describe('Check the gamepile exists', () => {
+    it('renders the gamepile', () => {
+        <DragDropContext onDragEnd={() => {}}>
+            <GamePile
+                pileId={"pile1"}
+                numId={"1"}
+                roomId={"1"}
+                key={"pile1"}
+                pileCards={[]}
+                create_deck={'create_deck'}
+                isDragging={false}
+                draw_multiple={'draw_multiple'}
+                handId={'1'}
+                deck={null}
+            />
+        </DragDropContext>
+    })
+})
