591cc419053778537ea2d25809b817463fd003f7
mschodin <michael-schodin@uiowa.edu>
2020-12-12 13:57:52 -0600

    Created tests for gamecard

--- spec/javascripts/gamecard.spec.js
+++ spec/javascripts/gamecard.spec.js
@@ -1,7 +1,29 @@
 import * as React from "react";
-import {render} from "@testing-library/react";
 import GameCard from "../../app/javascript/components/GameCard";
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import {cleanup, render} from "@testing-library/react";
 
-test('Check that a GameCard exists', () => {
+describe('Check the card is rendered correctly', () => {
+    it('renders the card as an image', () => {
+        const {container, getByAltText} = render(
+            <DragDropContext onDragEnd={() => {}}>
+                <Droppable>
+                    <GameCard face={'2'} suit={'C'} cardId={'card1'} index={1} key={'card1'}/>
+                </Droppable>
+            </DragDropContext>
+        )
 
-});
+        expect(getByAltText('2C')).toBeDefined()
+    })
+    it('renders the card as an image', () => {
+        const {container, getByAltText} = render(
+            <DragDropContext onDragEnd={() => {}}>
+                <Droppable>
+                    <GameCard face={'A'} suit={'S'} cardId={'card1'} index={1} key={'card1'}/>
+                </Droppable>
+            </DragDropContext>
+        )
+
+        expect(getByAltText('AS').nodeName).toBe('IMG')
+    })
+})
