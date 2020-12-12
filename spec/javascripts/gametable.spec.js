e2ac2a6c9e16cc86471c7d018aa26ce6085a3963
awashington <antonio-washington@uiowa.edu>
2020-12-12 15:34:41 -0600

    fixed rendering bug with gametable spec

--- spec/javascripts/gametable.spec.js
+++ spec/javascripts/gametable.spec.js
@@ -5,7 +5,22 @@ import GameRoom from "../../app/javascript/components/GameRoom";
 test('Check that GameTable exists', () => {
     const test_piles =  [[0,[["A", "S", 0],["A", "C", 1],["A", "H", 2]]],[ 1, [["A", "S", 0],["A", "C", 1],["A", "H", 2]]]]
     const test_hand =  [["A", "S", 0],["A", "C", 1],["A", "H", 2]]
-    const { getAllByRole, getByText, getByLabelText, getByTestId} = render(<GameRoom handId={0} playerHand={test_hand} piles={test_piles} />);
+    const test_ptd = {0:[], 1:[]}
+    const test_players = {"Juan":0 }
+    const test_player = {"name": "Juan" }
+    const { getAllByRole, getByText, getByLabelText, getByTestId} = render(
+        <GameRoom
+            handId={0}
+            playerHand={test_hand}
+            piles={test_piles}
+            piles_to_deck={ test_ptd }
+            create_deck_urls={"example_url/create_deck/__pile_id__"}
+            roomId={0}
+            players={test_players}
+            draw_multiple={"example_url/draw_multiple"}
+            room_passcode={"CODE"}
+            player={test_player}
+        />);
     expect(getAllByRole('table').length).toBe(1)
     expect(getAllByRole('pile').length).toBe(test_piles.length)
 });
