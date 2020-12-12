8603c2e30045d59f76fa38bc98314eeb6f59764e
Alec Cleofe <alec-cleofe@uiowa.edu>
2020-12-11 18:35:32 -0600

    Removed Unsubscrube fromthe activity channel

--- app/channels/activity_channel.rb
+++ app/channels/activity_channel.rb
@@ -3,8 +3,4 @@ class ActivityChannel < ApplicationCable::Channel
     # stream_from "some_channel"
     stream_from "activity_channel"
   end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
 end
