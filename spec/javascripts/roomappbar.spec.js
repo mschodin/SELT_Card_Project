ef202c425501c0b0abab40c6f2bf9e78a8e21767
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-12 22:31:30 -0600

    finished testing for appbar

--- spec/javascripts/roomappbar.spec.js
+++ spec/javascripts/roomappbar.spec.js
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
 import RoomAppBar from "../../app/javascript/components/RoomAppBar";
-import Home from "../../app/javascript/components/Home";
-import { shallow, mount } from 'enzyme';
+import userEvent from '@testing-library/user-event'
 
 describe('rendering the homepage application', ()=> {
     it("Should display the homepage header elements", () => {
@@ -11,7 +11,20 @@ describe('rendering the homepage application', ()=> {
         const test_passcode = "abcd";
 
         render(<RoomAppBar greeting={test_greeting} room_passcode={test_passcode} room_id={test_room_id} />);
+
         expect(screen.getByText(test_greeting)).toBeInTheDocument();
-        expect(screen.getByText(test_passcode)).toBeInTheDocument();
+        expect(screen.getByText("Room Code: "+test_passcode)).toBeInTheDocument();
     });
+
+    it("Should display Leave/End Game buttons when clicked", ()=>{
+        const test_greeting = "test greeting";
+        const test_room_id = 1;
+        const test_passcode = "abcd";
+
+        render(<RoomAppBar greeting={test_greeting} room_passcode={test_passcode} room_id={test_room_id} />);
+
+        userEvent.click(screen.getByLabelText('simple-menu'))
+        expect(screen.getByText('Leave Game')).toBeInTheDocument()
+        expect(screen.getByText('End Game')).toBeInTheDocument()
+    })
 });
