79fd4dcdc589f04a45d8b16ef6500c64b7f4453f
MBiggs18 <root@DESKTOP-E8KJCMN.localdomain>
2020-12-12 22:31:03 -0600

    finished landing page testing

--- spec/javascripts/landingpage.spec.js
+++ spec/javascripts/landingpage.spec.js
@@ -1,10 +1,7 @@
 import * as React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
-import HomeAppBar from "../../app/javascript/components/HomeAppBar";
-import { shallow, mount } from 'enzyme';
+import { render, screen } from '@testing-library/react'
 import Home from "../../app/javascript/components/Home";
-import {TextField} from "@material-ui/core";
 
 describe('Check that text displays when homepage is rendered', ()=>{
     it("Should display Create/Join Game text", ()=>{
@@ -14,28 +11,11 @@ describe('Check that text displays when homepage is rendered', ()=>{
         expect(screen.getAllByText('Create Game').length).toBe(2);
         expect(screen.getAllByText('Join Game').length).toBe(2);
     })
+
+    it("Should display the Create/Join Game submit buttons", ()=>{
+        const test_flash_message = null;
+        const {room_join_url, room_index_url} = "";
+        render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
+        expect(screen.getAllByLabelText('submit').length).toBe(2)
+    })
 })
-
-
-test("Check that onChange updates text field", ()=>{
-    // const test_flash_message = null;
-    // const {room_join_url, room_index_url} = "";
-    // render(<Home room_join={room_join_url} room_create={room_index_url} flash_message={test_flash_message}/>);
-    //
-    // const onChangeMock = jest.fn();
-    // const event = {
-    //     target: { value: 'test' }
-    // }
-    // const component = mount<TextField>(<TextField onChange={onChangeMock} />);
-    // const instance = component.instance();
-    // instance.onChange(event);
-    // expect(onChangeMock).toBeCalledWith('test');
-});
-
-test('calls onSubmit prop function when form is submitted', () => {
-    // const onSubmitFn = jest.fn();
-    // const wrapper = mount(<form onSubmit={onSubmitFn}/>);
-    // const form = wrapper.find('form');
-    // form.simulate('submit');
-    // expect(onSubmitFn).toHaveBeenCalledTimes(1);
-});
