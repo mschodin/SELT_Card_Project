c8c31a78c212db5477614b203e6073fa66760696
senortonito <senortonito@gmail.com>
2020-12-13 00:00:47 -0600

    testing moved over to jest

--- features/step_definitions/deck_steps.rb
+++ features/step_definitions/deck_steps.rb
@@ -1,32 +1,32 @@
-Given(/^I am in a room$/) do
+Given(/^I am in a room$/) do pending
   visit room_index_path
-  page.fill_in "create_name_box", :with => "UncommonName"
+  page.fill_in "create_name", :with => "UncommonName"
   click_button('Create Game')
   click_button('New Pile')
 end
 
-When(/^I click create a new deck$/) do
+When(/^I click create a new deck$/) do pending
   click_button('New Deck in Pile 1')
 end
 
-Then(/^I should see that a new deck is in the room$/) do
+Then(/^I should see that a new deck is in the room$/) do pending
   expect(page).to have_content("DECKS IN PILE: 1")
 end
 
 
-Given(/^I am on the game room home page AND a deck has been created$/) do
+Given(/^I am on the game room home page AND a deck has been created$/) do pending
   visit room_index_path
-  page.fill_in "create_name_box", :with => "UncommonName"
+  page.fill_in "create_name", :with => "UncommonName"
   click_button('Create Game')
   click_button('New Pile')
   click_button('New Deck in Pile 1')
 end
 
-When(/^I click Draw (\d+) Card$/) do |arg|
+When(/^I click Draw (\d+) Card$/) do |arg| pending
   click_button('Draw 1 Card')
 end
 
-Then(/^I should see the card that was drawn$/) do
+Then(/^I should see the card that was drawn$/) do pending
   cards =  page.all('td').map { |td| td.text }
   expect(cards).to include match "Suit"
   expect(cards).to include match "Rank"
@@ -36,23 +36,23 @@ Then(/^I should see the card that was drawn$/) do
   expect(ranks).to include cards[2]
 end
 
-Then(/^I should not see the Draw (\d+) Card button$/) do |arg|
+Then(/^I should not see the Draw (\d+) Card button$/) do |arg| pending
   expect(page).not_to have_button('Draw 1 Card')
 end
 
-Given(/^I am on the draw card room home page$/) do
+Given(/^I am on the draw card room home page$/) do pending
   visit room_index_path
-  page.fill_in "create_name_box", :with => "UncommonName"
+  page.fill_in "create_name", :with => "UncommonName"
   click_button('Create Game')
   click_button('New Pile')
   click_button('New Deck in Pile 1')
   click_button("Draw 1 Card from Pile 1")
 end
 
-When(/^I click link Return to Room$/) do
+When(/^I click link Return to Room$/) do pending
   click_link("Return to Room")
 end
 
-Then(/^I should see (\d+) less card in the deck$/) do |arg|
+Then(/^I should see (\d+) less card in the deck$/) do |arg| pending
   expect(page).to have_content("CARDS IN FIRST DECK: " + (RubyCards::Deck.new.count - 1).to_s)
 end
--- features/step_definitions/room_steps.rb
+++ features/step_definitions/room_steps.rb
@@ -1,17 +1,18 @@
-Given /^I am on the game room home page$/ do
+Given /^I am on the game room home page$/ do pending
   visit room_index_path
 end
 
-Given /^A room has been created$/ do
+Given /^A room has been created$/ do pending
   visit room_index_path
-  page.fill_in "create_name_box", :with => "UncommonName"
+  page.fill_in "create_name", :with => "UncommonName"
+  page.
   click_button('Create Game')
 end
 
-Given /^A room with id 1 has been created with player named "(.*?)" present$/ do |name|
+Given /^A room with id 1 has been created with player named "(.*?)" present$/ do |name| pending
   visit room_index_path
   if Room.last.nil? || Room.last.id.eql?(0)
-    page.fill_in "create_name_box", :with => name
+    page.fill_in "create_name", :with => name
     click_button('Create Game')
   else
     code = Room.exists?(id) ? Room.find(id).code : ""
@@ -22,19 +23,19 @@ Given /^A room with id 1 has been created with player named "(.*?)" present$/ do
   end
 end
 
-When /^I click create a new room with name "(.*?)"$/ do |name|
-  page.fill_in "create_name_box", :with => name
+When /^I click create a new room with name "(.*?)"$/ do |name| pending
+  page.fill_in "create_name", :with => name
   click_button('Create Game')
 end
 
-Then /^I should be placed in a new game room with name "(.*?)"$/ do |name|
+Then /^I should be placed in a new game room with name "(.*?)"$/ do |name| pending
   url = URI.parse(current_url)
   unique_id = Room.last.id.to_s
   expect(url.path).to eq('/room/' + unique_id)
   page.should have_content(name + ' is in private room ')
 end
 
-When /^I click join a room with name "(.*?)" and room id "(.*?)"$/ do |name, id|
+When /^I click join a room with name "(.*?)" and room id "(.*?)"$/ do |name, id| pending
   code = Room.exists?(id) ? Room.find(id).code : ""
   page.fill_in "join_name_box", :with => name
   page.fill_in 'room_id_box', :with => id
@@ -42,45 +43,45 @@ When /^I click join a room with name "(.*?)" and room id "(.*?)"$/ do |name, id|
   click_button('Join Game')
 end
 
-Then /^I should be placed in the game room with id "(.*?)"$/ do |id|
+Then /^I should be placed in the game room with id "(.*?)"$/ do |id| pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room/' + id)
 end
 
-Then /^The user is notified their name is invalid$/ do
+Then /^The user is notified their name is invalid$/ do pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
   page.should have_content('Name is invalid, please try again')
 end
 
-Then /^The user is notified the room id is invalid$/ do
+Then /^The user is notified the room id is invalid$/ do pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
   page.should have_content('Room id invalid, please try again')
 end
 
-Then /^The user is notified the room does not exist$/ do
+Then /^The user is notified the room does not exist$/ do pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
   page.should have_content('Room does not exist, please try again')
 end
 
-Then /^The user is notified a player with name "(.*?)" already exists$/ do |name|
+Then /^The user is notified a player with name "(.*?)" already exists$/ do |name| pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
   page.should have_content("Player with name " + name + " already exists in room 1")
 end
 
-When /^I click the leave game button$/ do
+When /^I click the leave game button$/ do pending
   click_button('Leave Game')
 end
 
-Then /^I should be redirected to the landing page$/ do
+Then /^I should be redirected to the landing page$/ do pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
 end
 
-Then(/^I should see the room with a list of players and their hand size$/) do
+Then(/^I should see the room with a list of players and their hand size$/) do pending
   table_list = page.all('table td:nth-child(2)').map{|td| td.text}
   table_list = table_list.map {|x| x[/\d+/]}
   expect(table_list.count).to be >= 1
@@ -89,35 +90,35 @@ Then(/^I should see the room with a list of players and their hand size$/) do
   end
 end
 
-And(/^I should NOT see the "(.*?)" in the list$/) do |name|
+And(/^I should NOT see the "(.*?)" in the list$/) do |name| pending
   table_list = page.all('table td:nth-child(1)').map{|td| td.text}
   expect(table_list).to_not include(name)
 end
 
-Then(/^I should see the room with a an empty list of players$/) do
+Then(/^I should see the room with a an empty list of players$/) do pending
   table_list = page.all('table td:nth-child(1)').map{|td| td.text}
   expect(table_list).to eq([])
 end
 
-When /^I click join a room with name "(.*?)", room id "(.*?)", and room code "(.*?)"$/ do |name, id, code|
+When /^I click join a room with name "(.*?)", room id "(.*?)", and room code "(.*?)"$/ do |name, id, code| pending
   page.fill_in "join_name_box", :with => name
   page.fill_in 'room_id_box', :with => id
   page.fill_in 'room_code_box', :with => code
   click_button('Join Game')
 end
 
-Then /^The user is notified the room code is invalid$/ do
+Then /^The user is notified the room code is invalid$/ do pending
   url = URI.parse(current_url)
   expect(url.path).to eq('/room')
   page.should have_content('Room code invalid, please try again')
 end
 
-And /^I should see the room code$/ do
+And /^I should see the room code$/ do pending
   code = Room.last.code
   div = find('[data-react-class="RoomAppBar"]') #gets react component RoomAppBar
   div[:'data-react-props'].should have_content('Room-code: ' + code)
 end
 
-When(/^I click End Game$/) do
+When(/^I click End Game$/) do pending
   click_button('End Game')
 end
